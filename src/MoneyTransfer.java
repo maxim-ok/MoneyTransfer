973ca19c2d628910f17661cf7f32143ac6fe4048
Maksim Okunev <max.vic.okunev@gmail.com>
2021-12-12 22:24:21 +0300

    Added TestingReport

--- src/MoneyTransfer.java
+++ src/MoneyTransfer.java
@@ -1,9 +1,9 @@
 public class MoneyTransfer {
     public static void main(String[] args) {
-        int currentAccountSumm = 2_000_000_000;
-        int transferSumm = 500_000_000;
-        int totalAccountSumm = transferSumm + currentAccountSumm;
+        int currentAccountSum = 2_000_000_000;
+        int transferSum = 500_000_000;
+        int totalAccountSum = transferSum + currentAccountSum;
 
-        System.out.println("Итого на счете " + totalAccountSumm + " попугаев");
+        System.out.println("Итого на счете " + totalAccountSum + " попугаев");
     }
 }
